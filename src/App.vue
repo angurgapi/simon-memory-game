d726f2a98075c206a5960c6eee974115d01ea7ea
angurgapi <sophiekovalsky@gmail.com>
2021-04-23 00:59:44 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+   
+    <Game lvl=0 />
+  </div>
+</template>
+
+<script>
+import Game from './components/Game.vue'
+
+export default {
+  name: 'simon',
+  components: {
+    Game
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Game.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="game">
+    <h1>level {{ lvl }}</h1>
+    <p>
+      difficulty
+    </p>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Game',
+  props: {
+    lvl: Number
+  }
+}
+</script>
+<style lang='sass' scoped>
+.game
+  background-color: rgba(0,0,0,.3)
+</style>
